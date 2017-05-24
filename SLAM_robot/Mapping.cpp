23a67b005ecbad8ae2069280b32942d7a7b43695
Seungmin Lee <Seungmin Lee>
2017-05-25 11:04:46 +1200

    mapping initial

---
+++ SLAM_robot/Mapping.cpp
@@ -0,0 +1,31 @@
+/*
+ * Mapping.cpp
+ *
+ *  Created on: May 25, 2017
+ *      Author: seungmin
+ */
+
+#include "Mapping.h"
+
+
+Mapping::Mapping() {
+  currxPos = 0;
+  curryPos = 0;
+}
+
+Mapping::~Mapping() {
+	// TODO Auto-generated destructor stub
+}
+
+float robotPosition(float xPos, float yPos, float radAngle) {
+  
+}
+
+float obstaclePosition(float xPos, float yPos, float *IRsensors, float sonar) {
+  
+}
+
+void sendInfo() {
+  
+}
+
